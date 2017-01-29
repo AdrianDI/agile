67e3239bfa116252ccf4c8e215fa4815033e4009
AdrianDI <a00200608@student.ait.ie>
2017-01-29 17:31:42 +0000

    first commit

---
+++ agile_ca/src/main/java/com/maven/agile_ca/App.java
@@ -0,0 +1,13 @@
+package com.maven.agile_ca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
